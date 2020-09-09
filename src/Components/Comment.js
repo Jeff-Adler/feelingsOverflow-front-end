2c8a5e4ec3a45afa0de23e0b4865f1655ff6f9fe
Jeff-Adler <Jeffm.adler@gmail.com>
2020-09-09 10:29:22 -0400

    added disabledVoteArrows

--- src/Components/Comment.js
+++ src/Components/Comment.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import upvoteArrow from './Images/upvote_arrow.png'
-import downvoteArrow from './Images/downvote_arrow.png'
+import upvoteEnabled from './Images/upvoteEnabled.png'
+import downvoteEnabled from './Images/downvoteEnabled.png'
+import upvoteDisabled from './Images/upvoteDisabled.png'
+import downvoteDisabled from './Images/downvoteDisabled.png'
 
 class Comment extends React.Component {
 
@@ -20,18 +22,18 @@ class Comment extends React.Component {
                 {`${this.props.comment.comment} Commenter: ${this.props.comment.commenter_id}`}<br/>
                 {this.checkIfVoted() 
                 ? 
-                    <img id="upvote" src={upvoteArrow} alt="upvote"/>
+                    <img id="upvote" src={upvoteDisabled} alt="upvote"/>
                 :
-                    <img id="upvote" onClick={event => this.props.voteHandler(event.target.id, this.props.comment)} src={upvoteArrow} alt="upvote"/>
+                    <img id="upvote" onClick={event => this.props.voteHandler(event.target.id, this.props.comment)} src={upvoteEnabled} alt="upvote"/>
                 }
                 {`\xa0`}{`\xa0`}
                 <strong>{this.props.comment.vote_tally}</strong>
                 {`\xa0`}{`\xa0`}
                 {this.checkIfVoted() 
                 ? 
-                    <img id="downvote" src={downvoteArrow} alt="downvote"/>
+                    <img id="downvote" src={downvoteDisabled} alt="downvote"/>
                 :
-                    <img id="downvote" onClick={event => this.props.voteHandler(event.target.id, this.props.comment)} src={downvoteArrow} alt="downvote"/>
+                    <img id="downvote" onClick={event => this.props.voteHandler(event.target.id, this.props.comment)} src={downvoteEnabled } alt="downvote"/>
                 }                       
             </>
         )
